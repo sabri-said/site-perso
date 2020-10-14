4e0e3d2a4d5524148a2831eea65eed47da84670c
Sabri SAID <sabri.said@laplateforme.io>
2020-10-14 16:58:48 +0200

    coming soon page
    website in progress

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/style/coming_soon.css">
+    <title>Coming Soon</title>
+</head>
+<body>
+<header></header>
+<!--<main>-->
+<div class="bgimg">
+    <div class="topleft">
+        <p>Sabri SAID</p>
+    </div>
+    <div class="middle">
+        <h1>COMING SOON</h1>
+        <hr>
+        <p id="countDown"></p>
+    </div>
+    <div class="bottomleft">
+        <p>Website in progress</p>
+    </div>
+</div>
+<!--</main>-->
+<footer></footer>
+<script src="assets/js/coming_soon_countdown.js"></script>
+</body>
+</html>
+
